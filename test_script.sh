b352188a95e94d6f9b9f7b1fd44d28f623ae1ae9
Sterling1111 <sterlingijeppson@gmail.com>
2026-04-11 23:57:47 -0700

    Remove unused files: commands.txt, fa_test.png, and test_script.sh

--- test_script.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-f="Calculus/Chapter18/Problem_18_4/fc.gp"
-base="fc"
-func="fc(x) = exp x + exp(-x)"
-
-extra_plots=", exp(x) title 'exp x' with lines linecolor rgb 'green', exp(-x) title '1/exp x' with lines linecolor rgb 'blue'"
-sed "s|notitle with filledcurves|title '${func}' with filledcurves${extra_plots}|" "$f" | grep plot
