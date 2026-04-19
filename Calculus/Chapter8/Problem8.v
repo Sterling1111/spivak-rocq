2036473ffae21d2f4a35487d59496e11b7533149
Sterling1111 <sterlingijeppson@gmail.com>
2026-04-19 16:35:12 -0700

    refactor: restructure lemmas in Chapter 8 for clarity and consistency

--- Calculus/Chapter8/Problem8.v
+++ Calculus/Chapter8/Problem8.v
@@ -1,17 +1,27 @@
 From Calculus.Chapter8 Require Import Prelude.
 
-Lemma lemma_8_8_a : ∀ f,
-  (∀ a b, a < b -> f a <= f b) ->
-  ∀ a, (∃ L, left_limit f a L) /\ (∃ L, right_limit f a L).
-Proof. Abort.
+Section section_8_8.
 
-Lemma lemma_8_8_b : ∀ f,
-  (∀ a b, a < b -> f a <= f b) ->
-  ∀ a, (∃ L1 L2, left_limit f a L1 /\ right_limit f a L2 /\ L1 <> L2) \/ continuous_at f a.
-Proof. Abort.
+Variable f : ℝ → ℝ.
+Hypothesis H1 : ∀ a b, a < b -> f a <= f b.
 
-Lemma lemma_8_8_c : ∀ f,
-  (∀ a b, a < b -> f a <= f b) ->
-  (∀ a b y, a < b -> f a < y < f b -> ∃ x, a < x < b /\ f x = y) ->
+Lemma lemma_8_8_a : ∀ a,
+  ∃ L1 L2, ⟦ lim a⁺ ⟧ f = L1 /\ ⟦ lim a⁻ ⟧ f = L2.
+Proof.
+  intros a.
+Abort.
+
+Lemma lemma_8_8_b : ∀ a : ℝ,
+  ¬ removably_discontinuous_at f a.
+Proof.
+  intros a H2.
+Abort.
+
+Lemma lemma_8_8_c :
+  (∀ a b c, a < b -> f a <= c <= f b -> ∃ x, x ∈ [a, b] /\ f x = c) ->
   continuous f.
-Proof. Abort.
+Proof.
+  intros H2.
+Abort.  
+
+End section_8_8.
